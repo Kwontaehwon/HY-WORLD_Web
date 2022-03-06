0a63b53bf904232394e4c9f931675b72845a1738
Kwontaehwon <kygkth2011@gmail.com>
2022-03-07 00:15:28 +0900

    Mobile direct to login

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, current_app, render_template, g
+from flask import Blueprint, url_for, current_app, render_template, g, request
 from werkzeug.utils import redirect
 from ..views.auth_views import login_required
 
@@ -7,10 +7,22 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def hello():
+
     if g.user != None :
         return redirect(url_for('question._list'))
     else :
-        return render_template('main.html')
+        user_agent = request.headers.get('User-Agent')
+        user_agent = user_agent.lower()
+
+        # In your templates directory, create a mobile version of your site (mobile.index.html).
+        # Likewise, add your desired desktop template as well (desktop.index.html).
+
+        if "iphone" in user_agent:
+            return redirect(url_for('auth.login'))
+        elif "android" in user_agent:
+            return redirect(url_for('auth.login'))
+        else:
+            return render_template('main.html')
 
 
 @bp.route('/index')
