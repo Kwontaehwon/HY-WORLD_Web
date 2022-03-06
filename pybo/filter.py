a736d89bafd2020cdb0dcc11ac3e4197926dd4b0
Kwontaehwon <kygkth2011@gmail.com>
2022-03-06 20:58:15 +0900

    Edit simplemde in mobile and change datetime filter

--- pybo/filter.py
+++ pybo/filter.py
@@ -1,2 +1,10 @@
-def format_datetime(value, fmt='%Y년 %m월 %d일 %H:%M'):
+import datetime
+
+
+def format_datetime(value):
+    today = datetime.datetime.now()
+    if value.year == today.year:
+        fmt = '%m/%d %H:%M'
+    else :
+        fmt = '%Y년 %m월 %d일 %H:%M'
     return value.strftime(fmt)
